50b880c5b2b9b24adeb6e0c567c884d55fb4a163
antaresmilo17 <ricardoclimo@live.com>
2022-04-22 16:09:05 -0700

    r-kzMUl0Gr-part2: (#5)
    * Added base_button script
    * Added play and quit button scripts
    * Play button is able to move to the main_game scene
    * Play button quits the application when running it in the .exe
    * Added title and derpy picture of dogs

---
+++ MyProject/Assets/scripts/script_templates/scenes/scene_manager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_manager : MonoBehaviour
+{
+    public void loadMainMenuScene(){
+        SceneManager.LoadScene("main_menu");
+    }
+
+    public void loadMainGameScene(){
+        SceneManager.LoadScene("main_game");
+    }
+}
